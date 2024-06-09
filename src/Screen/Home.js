f3bf0a3cf0dbf41760a01c51d002a506e0f2ad69
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-09 21:27:08 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./Home.css";
 import BgImg from "../Img/Emirates-airlines.jpg";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import {
   ArrowRightOutlined,
@@ -15,18 +18,61 @@ import {
   LeftOutlined,
 } from "@ant-design/icons";
 
+const options = [
+  { value: "option1", label: "Option 1" },
+  { value: "option2", label: "Option 2" },
+  { value: "option3", label: "Option 3" },
+  { value: "option4", label: "Option 4" },
+  // Add more options as needed
+];
+
 export default function Home() {
   const [page, setPage] = useState(1);
   const [flightsPage, setFlightsPage] = useState(1);
   const [passengerDropdownOpen, setPassengerDropdownOpen] = useState(false);
-  const [adults, setAdults] = useState(3);
-  const [children, setChildren] = useState(1);
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
   const [infants, setInfants] = useState(0);
   const [flightType, setFlightType] = useState("Any Flight");
   const [flightClass, setFlightClass] = useState("Economy");
 
   const totalPassengers = adults + children + infants;
 
+  // Leaving From
+  const [selectedOption, setSelectedOption] = useState(null);
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+  };
+
+  // Going From
+  const [selectedOption2, setSelectedOption2] = useState(null);
+  const handleChange2 = (selectedOption2) => {
+    setSelectedOption2(selectedOption2);
+  };
+
+  // Preferred Carrier
+  const [selectedOption3, setSelectedOption3] = useState(null);
+  const handleChange3 = (selectedOption3) => {
+    setSelectedOption3(selectedOption3);
+  };
+
+  // Galileo PK
+  const [selectedOption4, setSelectedOption4] = useState(null);
+  const handleChange4 = (selectedOption4) => {
+    setSelectedOption4(selectedOption4);
+  };
+
+  // Departure Date
+  const [selectedDate, setSelectedDate] = useState(null);
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  //Return Date
+  const [selectedDate2, setSelectedDate2] = useState(null);
+  const handleDateChange2 = (date) => {
+    setSelectedDate2(date);
+  };
   return (
     <div className="home-body">
       <img src={BgImg} alt="bg" className="home-img" />
@@ -264,7 +310,82 @@ export default function Home() {
                   </div>
                 </div>
 
-                <div>ggg</div>
+                {flightsPage === 1 ? (
+                  <div>Page-1</div>
+                ) : flightsPage === 2 ? (
+                  <div>Page-2</div>
+                ) : flightsPage === 3 ? (
+                  <div>Page-3</div>
+                ) : (
+                  <p>Error</p>
+                )}
+
+                <div>
+                  <div className="select-container">
+                    <Select
+                      value={selectedOption}
+                      onChange={handleChange}
+                      options={options}
+                      isSearchable
+                      placeholder="Leaving From"
+                      className="home-Select1"
+                    />
+
+                    <Select
+                      value={selectedOption2}
+                      onChange={handleChange2}
+                      options={options}
+                      isSearchable
+                      placeholder="Going From"
+                      className="home-Select1"
+                    />
+
+                    <div className="calendar-container">
+                      <p>Departure Date</p>
+                      <DatePicker
+                        selected={selectedDate}
+                        onChange={handleDateChange}
+                        dateFormat="dd/MM/yyyy" // Change the format as needed
+                        placeholderText="Select a date"
+                        isClearable
+                        className="home-DatePicker"
+                      />
+                    </div>
+
+                    <div className="calendar-container">
+                      <p>Return Date</p>
+                      <DatePicker
+                        selected={selectedDate2}
+                        onChange={handleDateChange2}
+                        dateFormat="dd/MM/yyyy" // Change the format as needed
+                        placeholderText="Select a date"
+                        isClearable
+                        className="home-DatePicker"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <Select
+                      value={selectedOption3}
+                      onChange={handleChange3}
+                      options={options}
+                      isSearchable
+                      placeholder="Preferred Carrier"
+                      className="home-Select1"
+                    />
+
+                    <Select
+                      value={selectedOption4}
+                      onChange={handleChange4}
+                      options={options}
+                      isSearchable
+                      placeholder="Galileo PK"
+                      className="home-Select1"
+                    />
+                  </div>
+                  <button>SEARCH</button>
+                </div>
               </div>
             ) : page === 2 ? (
               // Hotels
@@ -272,6 +393,17 @@ export default function Home() {
             ) : (
               <h2>Error page</h2>
             )}
+          </div>
+
+          <div >
+            <h3>Airline: </h3>
+            <h3>Details: </h3>
+            <h3>Departure: </h3>
+            <p>16-Jun-24 23:00</p>
+            <h3>Arrival:</h3>
+            <h3>Duration:</h3>
+            <h3>Baggage:</h3>
+            <h1>Price:</h1>
           </div>
         </div>
       </div>
